5898356ad1f01bf05eb2758e602a06f429b17740
ramkisaikam <50354258+ramkisaikam@users.noreply.github.com>
2022-05-24 17:27:36 +0530

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+
+def search(nums, target) :
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] > target:
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return -1
+
+List = [-1,0,1,2,6,7,9,15,22]
+target = 9
+index = search(nums=List,target=target)
+print(index)
